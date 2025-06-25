95c97d3bc80313307b34a7d139c084350c0026a9
EdmundoZ12 <edmundozentenor12@gmail.com>
2025-06-25 14:26:16 -0400

    finish

--- src/main/java/com/tecnoweb/grupo7sa/ConfigDB/ConfigDB.java
+++ src/main/java/com/tecnoweb/grupo7sa/ConfigDB/ConfigDB.java
@@ -8,20 +8,20 @@ public class ConfigDB {
     private final String dbName;
 
     public ConfigDB() {
-//        this.user = "grupo07sa";
-        this.user = "postgres";
+        this.user = "grupo07sa";
+//        this.user = "postgres";
 
-//        this.password = "grup007grup007*";
-        this.password = "ejzr1203";
+        this.password = "grup007grup007*";
+//        this.password = "ejzr1203";
 
-//        this.host = "mail.tecnoweb.org.bo";
-        this.host = "127.0.0.1";
+        this.host = "mail.tecnoweb.org.bo";
+//        this.host = "127.0.0.1";
 
 //        this.port = "5432";
         this.port = "5432";
 
-//        this.dbName = "db_grupo07sa";
-        this.dbName = "tecnodb";
+        this.dbName = "db_grupo07sa";
+//        this.dbName = "tecnodb";
 
     }
 
